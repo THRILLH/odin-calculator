fc0e3781bbf4addfc2065a2cd5a85642f601c3e4
Matt Seton <mmseton@gmail.com>
2024-09-03 08:47:30 -0300

    Write pseudocode

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// When the user clicks a number button
+// Display the number on screen and record it in a variable
+// If the user clicks another number button
+// Concat that number to the end of the previous one and display
+
+// When the user clicks an operator
+// Prime that operator as the one to be used and display
+
+// Show the next number(s) as in the first step, record and display
+
+// When the user clicks 'equals'
+// Run the calculation as displayed
