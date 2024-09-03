1aee3d5618418edc64eda730ee1cf8d5146e2c35
Matt Seton <mmseton@gmail.com>
2024-09-03 11:12:31 -0300

    Link number buttons and create functions for basic addition

--- javascript.js
+++ javascript.js
@@ -10,6 +10,10 @@
 
 // When the user clicks 'equals'
 // Run the calculation as displayed
+let operator;
+let result;
+let num1;
+let num2;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -17,13 +21,83 @@ function add(num1, num2) {
 
 
 
-function operate(num1, num2) {
+function operate(one, two) {
     if (operator === "add") {
-        return add(num1, num2)
+        one = num1;
+        two = num2;
+        result = add(one, two);
+        console.log(result);
+        return num1 = result;      
     }
 }
-let operator = "add";
-let num1 = 3;
-let num2 = 5;
 
-console.log(operate(num1, num2));
+function pressNumber(number) {
+    if (operator === undefined && result === undefined) return num1 = number;
+    else return num2 = number;
+}
+
+function pressOperator(input) {
+    return operator = input;
+}
+
+const btn1 = document.querySelector(".btn-1");
+btn1.addEventListener("click", () => {
+    pressNumber(1);
+});
+
+const btn2 = document.querySelector(".btn-2");
+btn2.addEventListener("click", () => {
+    pressNumber(2);
+});
+
+const btn3 = document.querySelector(".btn-3");
+btn3.addEventListener("click", () => {
+    pressNumber(3);
+});
+
+const btn4 = document.querySelector(".btn-4");
+btn4.addEventListener("click", () => {
+    pressNumber(4);
+});
+
+const btn5 = document.querySelector(".btn-5");
+btn5.addEventListener("click", () => {
+    pressNumber(5);
+});
+
+const btn6 = document.querySelector(".btn-6");
+btn6.addEventListener("click", () => {
+    pressNumber(6);
+});
+
+const btn7 = document.querySelector(".btn-7");
+btn7.addEventListener("click", () => {
+    pressNumber(7);
+});
+
+const btn8 = document.querySelector(".btn-8");
+btn8.addEventListener("click", () => {
+    pressNumber(8);
+});
+
+const btn9 = document.querySelector(".btn-9");
+btn9.addEventListener("click", () => {
+    pressNumber(9);
+});
+
+const btn0 = document.querySelector(".btn-0");
+btn0.addEventListener("click", () => {
+    pressNumber(0);
+});
+
+const btnPlus = document.querySelector(".btn-plus");
+btnPlus.addEventListener("click", () => {
+    pressOperator("add");
+})
+
+
+
+const btnEquals = document.querySelector(".btn-equals");
+btnEquals.addEventListener("click", () => {
+    operate(num1, num2);
+});
